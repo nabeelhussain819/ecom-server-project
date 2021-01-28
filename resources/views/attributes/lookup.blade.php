46539f59fced5a43509b8dbb2b851374f4b187ca
Armash <armashash@gmail.com>
2021-01-28 13:22:22 +0500

    attribute values

--- resources/views/attributes/lookup.blade.php
+++ resources/views/attributes/lookup.blade.php
@@ -1,4 +1,4 @@
-<select id="properties" name="property_id" class="form-control">
+<select id="properties" name="attribute_id" class="form-control">
     <option value="" selected>Please select...</option>
     @foreach( \App\Models\Attribute::getAll()->get() as $property)
         <option value="{{$property->id}}">{{$property->name}}</option>
