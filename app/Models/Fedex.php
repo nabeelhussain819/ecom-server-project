93142906893fb2ef9fa520888d160c2744d53b10
Syed Armash Hussain <armashash@gmail.com>
2022-07-28 01:33:08 +0500

    validate shipment api in progress

--- app/Models/Fedex.php
+++ app/Models/Fedex.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 use App\Helpers\HttpHelper;
+use Google\Service\CloudTasks\HttpRequest;
+use Google\Service\Docs\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Fedex
 {
     const API = 'https://apis-sandbox.fedex.com';
 
-    private static function authorize()
+    public static function authorize()
     {
         $url = self::API . '/oauth/token';
         $headers = ['Content-Type' => 'application/x-www-form-urlencoded'];
@@ -19,5 +22,83 @@ class Fedex
         ];
 
         return HttpHelper::request($url, $body, 'POST', $headers);
+    }
+
+
+    public static function validateShipment(string $token, array $data = []) 
+    {
+        $url = self::API . '/ship/v1/shipments/packages/validate';
+     
+        $headers = array();
+
+        $headers[] = 'Content-Type: application/json';
+        $headers[] = 'Authorization: Bearer '. $token;
+        $headers[] = 'X-locale: en_US';
+
+        return HttpHelper::request($url, $data, 'POST', $headers, true);
+    }
+
+    public static function dummyPayload() {
+        return array(
+            'requestedShipment' => [
+              'pickupType' => 'USE_SCHEDULED_PICKUP',
+              'serviceType' => 'PRIORITY_OVERNIGHT',
+              'packagingType' => 'YOUR_PACKAGING',
+              'shipper' => [
+                'address' => [
+                  'streetLines' => [
+                    0 => '10 FedEx Parkway',
+                    1 => 'Suite 302'
+                  ],
+                  'city' => 'Beverly Hills',
+                  'stateOrProvinceCode' => 'CA',
+                  'postalCode' => 90210,
+                  'countryCode' => 'US'
+                ],
+                'contact' => [
+                  'personName' => 'SHIPPER NAME',
+                  'phoneNumber' => 1234567890,
+                  'companyName' => 'Shipper Company Name'
+                ],
+              ],
+              'recipients' => [
+                'address' => [
+                  'streetLines' => '-10 FedEx Parkway -Suite 302',
+                  'city' => 'Beverly Hills',
+                  'stateOrProvinceCode' => 'CA',
+                  'postalCode' => 90210,
+                  'countryCode' => 'US',
+                ],
+                'contact' => [
+                  'personName' => 'SHIPPER NAME',
+                  'phoneNumber' => 9612345671,
+                  'companyName' => 'Shipper Company Name',
+                ],
+              ],
+              'shippingChargesPayment' => [
+                'paymentType' => 'SENDER',
+                'payor' => [
+                  'responsibleParty' => [
+                    'accountNumber' => [
+                      'value' => 'Your account number',
+                    ],
+                  ],
+                ],
+              ],
+              'labelSpecification' => [
+                'labelStockType' => 'PAPER_7X475',
+                'imageType' => 'PDF',
+              ],
+              'requestedPackageLineItems' => [
+                'weight' => [
+                  'units' => 'LB',
+                  'value' => 68,
+                ],
+              ],
+            ],
+            'accountNumber' => [
+              'value' => 'Your account number',
+            ],
+        );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api;
+use App\Models\Fedex;
 use Illuminate\Support\Facades\Route;
 
 /*
